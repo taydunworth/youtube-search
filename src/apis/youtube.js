5e9986b84909e0890caa1bd77788e54c0f524f2c
Taylor <taylorashtoncox@gmail.com>
2019-03-14 14:12:16 -0400

    Connects to YouTube API pulling in data with async promise on submit of form

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const KEY = 'ACCESS-KEY-HERE';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v2',
+    baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
-        part:: 'snippet',
+        part: 'snippet',
         maxResults: 5,
         key: KEY
     }
