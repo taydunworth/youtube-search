c2093b58d377b3173729a82f2ea97498784d9b5d
Taylor <taylorashtoncox@gmail.com>
2019-03-14 12:13:07 -0400

    Sets up search bar with event handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = (e) => {
+        this.setState({ term: e.target.value });
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <h1>SearchBar</h1>
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange} />
+                    </div>
+                </form>
+            </div>
         )
     };
 };
